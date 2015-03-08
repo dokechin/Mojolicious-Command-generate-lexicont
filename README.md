1d00e139b1f7dd656a4b591e2b1327bd7b142a00
dokechin <user@user-PC.(none)>
2015-03-09 08:07:06 +0900

    implement

--- README.md
+++ README.md
@@ -4,11 +4,32 @@ Mojolicious::Command::generate::lexicont - Mojolicious Lexicon Translation Gener
 
 # SYNOPSIS
 
-    use Mojolicious::Command::generate::lexicont;
+    APPLICATION generate lexicont src_lang dest_lang ...
 
 # DESCRIPTION
 
-Mojolicious::Command::generate::lexicont is lexicon translation generator for Mojolicious.
+Mojolicious::Command::generate::lexicont is lexicon translation generator.
+
+# CONFIGURATION
+
+Create config file lingua\_translate.conf on your project home directory.
+
+for example 
+
+Bing
+
+{
+  back\_end => "Bing",
+  client\_id => "YOUR\_CLIENT\_ID", 
+  client\_secret => "YOUR\_CLIENT\_SECRET"
+}
+
+Google
+
+{
+  back\_end => "Google",
+  apy\_key  => "YOUR\_API\_KEY",
+}
 
 # LICENSE
 
--- lib/Mojolicious/Command/generate/lexicont.pm
+++ lib/Mojolicious/Command/generate/lexicont.pm
@@ -3,23 +3,32 @@ use 5.008005;
 use strict;
 use warnings;
 use Mojo::Base 'Mojolicious::Command';
-
-our $VERSION = "0.01";
+use Lingua::Translate;
+use Config::PL;
+use Carp;
+use Data::Dumper;
 
 __PACKAGE__->attr(description => <<'EOF');
 Generate lexicon file translations.
 EOF
 
 __PACKAGE__->attr(usage => <<"EOF");
-usage: $0 generate lexicont src_lang dest_lang ...
+usage: APPLICATION generate lexicont src_lang dest_lang ...
 EOF
+
+__PACKAGE__->attr('conf_file');
+__PACKAGE__->attr('conf');
+
+our $VERSION = "0.01";
+
 
 sub run {
     my $self      = shift;
+    
+    my $arg_num = @_;
 
-    if ($@ < 2){
-        print "please specify arguments";
-        exit;
+    if ($arg_num < 2){
+        croak $self->usage;
     }
 
     my $src_lang  = shift;
@@ -41,24 +50,38 @@ sub run {
 
     my $src_file = $app->home->rel_file("lib/$app_class/I18N/${src_lang}.pm");
 
-    if (-e $src_file) {
-        print <<NOTFOUND;
-File not found $src_file
+    if (! -e $src_file) {
+        croak <<NOTFOUND;
+Src lexicon not found $src_file
 NOTFOUND
-        return;
     }
 
     my %srclex = eval {
         require "$app_class/I18N/${src_lang}.pm";
         no strict 'refs';
-        %{*{"${app_class}::I18N::${src_lang}::Lexicon"}};
+        %{*{"${app_klass}::I18N::${src_lang}::Lexicon"}};
+    };            
+    if ($@){
+        croak( "error $@" );
+    }
+
+    my $conf_file = $self->conf_file || "lingua_translate.conf";
+    my $conf;
+    eval{
+        $conf = config_do $conf_file;
     };
+    if ($@){
+        croak "Config file cannot read $@ ($conf_file)"
+    }
+    
+    $self->conf($conf);
 
     for my $dest_lang (@dest_langs){
 
         my $dest_file = $app->home->rel_file("lib/$app_class/I18N/${dest_lang}.pm");
+
+        my %lexicon = map { $_ => $self->translate( $src_lang, $dest_lang, $srclex{$_}) } keys %srclex;
         
-        my %lexicon = map { $_ => _translate( $srclex{$_}, $dest_lang) } keys %srclex;
 
         # Output lexem
         $self->render_to_file('package', $dest_file, $app_klass, $dest_lang,
@@ -68,14 +91,39 @@ NOTFOUND
 
 }
 
-sub _translate{
+sub translate{
 
-  my $src = shift;
-  my $lang = shift;
-  
-  return "dummy";
+    my $self = shift;
+    my $src = shift;
+    my $dest = shift;
+    my $text = shift;
+
+    my $xl8r;
+
+    eval{
+        $xl8r  = Lingua::Translate->new(%{$self->conf}, src => $src, dest => $dest);
+    };
+    if ($@){
+        croak "Lingua::Translate create error $@";
+    }
+    
+    my $trans_text = '';
+
+    eval{
+        $trans_text = $xl8r->translate($text);
+        if ($self->conf->{back_end} eq "InterTran"){
+            sleep(5);
+        }
+    };
+    if ($@){
+        warn ("Cannot translate $@");
+    }
+    return $trans_text;
 
 }
+
+1;
+
 __DATA__
 @@ package
 % my ($app_class, $language, $lexicon) = @_;
@@ -118,7 +166,28 @@ Mojolicious::Command::generate::lexicont - Mojolicious Lexicon Translation Gener
 
 =head1 DESCRIPTION
 
-Mojolicious::Command::generate::lexicont is lexicon translation generator for Mojolicious.
+Mojolicious::Command::generate::lexicont is lexicon translation generator.
+
+=head1 CONFIGURATION
+
+Create config file lingua_translate.conf on your project home directory.
+
+for example 
+
+Bing
+
+{
+  back_end => "Bing",
+  client_id => "YOUR_CLIENT_ID", 
+  client_secret => "YOUR_CLIENT_SECRET"
+}
+
+Google
+
+{
+  back_end => "Google",
+  apy_key  => "YOUR_API_KEY",
+}
 
 =head1 LICENSE
 
